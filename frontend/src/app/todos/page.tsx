5185b3b12c390bf63b3bef5b5365c9a8ee1689f2
tewarishash12 <tewarishashwat2002@gmail.com>
2025-04-09 19:59:39 +0530

    make the changes to check for deployment issues resolved

--- frontend/src/app/todos/page.tsx
+++ frontend/src/app/todos/page.tsx
@@ -2,29 +2,27 @@
 
 import TodoLayout from '@/app/components/TodoLayout';
 import { fetchTodos } from '@/app/lib/api';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Todo } from '../typecheck/typeCheck';
 
 export default function TodosPage() {
     const [todo, setTodo] = useState<Todo[]>([]);
 
-    useEffect(() => {
-        async function getTodos() {
-            try {
-                const tasks = await fetchTodos();
-                setTodo(tasks.todos);
-            } catch (err: unknown) {
-                if (err instanceof Error) {
-                    console.error(err.message);
-                } else {
-                    console.error('An unexpected error occurred', err);
-                }
+    async function getTodos() {
+        try {
+            const tasks = await fetchTodos();
+            setTodo(tasks.todos);
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                console.error(err.message);
+            } else {
+                console.error('An unexpected error occurred', err);
             }
         }
-        getTodos();
-    }, []);
+    }
+    getTodos();
 
-    console.log(todo);
+    // console.log(todo);
 
-    return <TodoLayout todos={todo} />;
+    return <TodoLayout todos={todo || []} />;
 }
